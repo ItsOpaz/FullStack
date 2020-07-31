d529459ea63240992d06ce88236995eb9add35a2
ItsOpaz <olli.pettersson@outlook.com>
2020-07-31 20:51:31 +0300

    osa4 list_helper testi

---
+++ osa4/tests/list_helper.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
