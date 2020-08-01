f26a3419b1ff8f0edf93113e40ec467e92d829c4
ItsOpaz <olli.pettersson@outlook.com>
2020-08-01 20:33:09 +0300

    lisätty blogin muokkaus ja poisto ja testit näille

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -41,27 +41,22 @@ blogsRouter.post('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
-
-  const blog = {
-    content: body.content,
-    important: body.important,
+  const blog = await Blog.findById(request.params.id)
+  const updatedBlog = {
+    title: body.title ? body.title : blog.title,
+    author: body.author ? body.author : blog.author,
+    url: body.url ? body.url : blog.url,
+    likes: body.likes ? body.likes : blog.likes
   }
-
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedblog => {
-      response.json(updatedblog.toJSON())
-    })
-    .catch(error => next(error))
+  await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -12,12 +12,12 @@ const emptyURL = {title: 'TEST', author: 'TEST', likes: 0}
 
 
 const blogs = [
-  { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0, user: '5f049c89e106ab568c056b31' },
-  { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0, user: '5f049c89e106ab568c056b31' },
-  { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0, user: '5f049c89e106ab568c056b31' },
-  { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0, user: '5f049c89e106ab568c056b31'  },
-  { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0, user: '5f049c89e106ab568c056b31'  },
-  { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2, __v: 0, user: '5f049c89e106ab568c056b31' }
+  { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0},
+  { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0},
+  { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0},
+  { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0},
+  { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0},
+  { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2, __v: 0 }
 ]
 
 beforeEach(async () => {
@@ -80,6 +80,30 @@ test('empty URL results in bad request', async() => {
     .post('/api/blogs')
     .send(emptyURL)
     .expect(400)
+})
+
+test('updating blog works', async() => {
+  const updatedBlog = await api
+    .put('/api/blogs/5a422a851b54a676234d17f7')
+    .send({author: "test author"})
+  const check = await api
+    .get('/api/blogs/5a422a851b54a676234d17f7')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(check.body).toEqual({ id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'test author', url: 'https://reactpatterns.com/', likes: 7})
+})
+
+test('deleting blog returns code 204 and one blog is removed', async() => {
+  const control = await api
+    .get('/api/blogs')
+    .expect('Content-Type', /application\/json/)
+  await api
+    .delete('/api/blogs/5a422a851b54a676234d17f7')
+    .expect(204)
+  const check = await api
+    .get('/api/blogs')
+    .expect('Content-Type', /application\/json/)
+  expect(check.body).toHaveLength(control.body.length - 1)
 })
 
 
