305ed2b07755ac8844447d560d014f0031825a68
ItsOpaz <olli.pettersson@outlook.com>
2020-08-04 15:46:22 +0300

    4.21 asti toimiva, testit ei toimi

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
@@ -14,20 +15,18 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.user)
-  if (typeof body.likes === 'undefined'){
-    body.likes = 0
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
   }
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
     url: body.url,
     title: body.title,
     author: body.author,
-    user: user._id,
-    likes: body.likes
+    user: user._id ? user._id : (await User.findOne().toJSON().id),
+    likes: body.likes ? body.likes : 0
   })
-  if (body.likes === null){
-    blog.likes = 0
-  }
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
@@ -35,6 +34,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const blog = await Blog.findById(request.params.id)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }else if (decodedToken.id.toString() !== blog.user.toString()) {
+    return response.status(403).end()
+  }
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
